06c849af9f69afde0ba04fa9f6948b44a2344702
abakaushik-lgtm <abakaushik@users.noreply.github.com>
2026-06-18 23:38:10 +0530

    Fix CORS and gitignore

--- backend/main.py
+++ backend/main.py
@@ -34,7 +34,7 @@ app = FastAPI(
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=settings.CORS_ORIGINS,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
